a6fa76a466a454dd6fba976c015dc4113c1cdb67
Raimeeab <raimee.abbassi@gmail.com>
2021-11-05 19:13:39 +1100

    Revert changes to router code

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,21 +1,18 @@
 // Dependencies
+const router = require('express').Router();
 const noteDb = require('../db/db.json');
 const fs = require('fs');
+
+router.get('/notes', function(req, res){
+    //get notes from db.json
+    let readNotes = fs.readFile(noteDb, "utf-8"); 
+    let parseNotes = JSON.parse(readNotes); // will get an array of strings 
+
+    return res.json(paresNotes);
+});
 
 // router.post('/notes', function(req, res){
 //     // save notes to db.json
 //     let saveNotes = 
 // })
-module.exports = function(app) {
-    app.get('/notes', function(req, res){
-        //get notes from db.json
-        let readNotes = fs.readFile(noteDb, "utf-8"); 
-        let parseNotes = JSON.parse(readNotes); // will get an array of strings 
-    
-        return res.json(paresNotes);
-    });
-
-    // app.get('/notes', function(req, res) {
-    //     let newNote = 
-    // })
-};
+module.exports = router;
--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,13 +1,15 @@
 // Dependencies
 const path = require('path');
+const router = require('express').Router();
 
 // API routes to HTML pages 
-module.exports = function(app){
-    app.get('/notes', function(req, res){
-        res.sendFile(path.join(__dirname, '../public/notes.html'));
-    });
-    
-    app.get('*', function(req, res){
-        res.sendFile(path.join(__dirname, '../public/index.html'));
-    });
-};
+
+router.get('/notes', function(req, res){
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+router.get('*', function(req, res){
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+module.exports = router;
