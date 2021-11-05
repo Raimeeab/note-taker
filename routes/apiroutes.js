81f9b4eef62a69e24605862b6ec8002f84470e17
Raimeeab <raimee.abbassi@gmail.com>
2021-11-05 20:11:09 +1100

    Add index.js file, delete htmlroutes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -8,11 +8,14 @@ router.get('/notes', function(req, res){
     let readNotes = fs.readFile(noteDb, "utf-8"); 
     let parseNotes = JSON.parse(readNotes); // will get an array of strings 
 
-    return res.json(paresNotes);
+    return res.json(parseNotes);
 });
 
 // router.post('/notes', function(req, res){
 //     // save notes to db.json
-//     let saveNotes = 
+//     let notes = JSON.parse(data);
+//     let saveNotes = fs.writeFile(noteDb, JSON.stringify(notes))
+
+//     return 
 // })
 module.exports = router;
--- server.js
+++ server.js
@@ -2,8 +2,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const htmlroutes = require("./routes/htmlroutes.js");
-const apiroutes = require("./routes/apiroutes");
+const routes = require("./routes");
 
 // Sets up the Express App
 const app = express();
@@ -15,32 +14,8 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 // API routes
-app.use('/', htmlroutes);
-app.use('/api', apiroutes);
-
-
-// // DB filename
-// const dbFileName = path.join(__dirname, "/db/db.json");
-
-// // Routes
-// app.get("/notes", function(req, res) {
-//     res.sendFile(path.join(__dirname, "/public/notes.html"));
-// });
-
-// app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
-
-// app.get('/api/notes', function(req, res){
-//   //get notes from db.json
-//   let readNotes = fs.readFile(dbFileName, "utf-8"); 
-//   let parseNotes = JSON.parse(readNotes); // will get an array of strings 
-
-//   return res.json(paresNotes);
-// });
-
-// app.get("*", function(req, res) {
-//     res.sendFile(path.join(__dirname, "/public/index.html"));
-// });
+app.use(routes);
 
 app.listen(PORT, function() {
-    console.log(`App listening at http://localhost:${PORT}`);
+    console.log(`App listening at http://localhost:${PORT} 🚀`);
 });
